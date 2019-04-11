4ad8e0c16c391b2dbf931a33555c38e6fb53281b
Dave Kruse <dave@krusetech.com>
2019-04-10 22:21:25 -0500

    add fetch and render results

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.scss';
 import { css } from '@emotion/core';
-import { CircleLoader } from 'react-spinners';
+import { PacmanLoader } from 'react-spinners';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      loading: true
+      loading: true,
+      pokemonData: []
     }
   }
 
+  componentDidMount() {
+    fetch('https://pokeapi.co/api/v2/pokemon') // returns a response which contains json
+      .then(
+        response => response.json() // returns javascript objects (from json)
+      ).then(
+        data => {
+          this.setState(
+            {
+              pokemonData: data.results,
+              loading: false
+            }
+          )
+        }
+      )
+  }
 
   render() {
+
+    let pokemonComponentList = this.state.pokemonData.map(
+      pokemon => <div>{pokemon.name}</div>
+    )
+
     return (
-      <div className="loader">
-        <CircleLoader
-            sizeUnit={"px"}
-            size={50}
-            color={'#123abc'}
-            loading={this.state.loading}
-          />
+      <div className="app">
+        {this.state.loading && //short circuit
+          <div className="loader">
+            <PacmanLoader
+              sizeUnit={"px"}
+              size={50}
+              color={'#123abc'}
+              loading={this.state.loading}
+            />
+          </div>
+        }
+        {pokemonComponentList}
       </div>
-    );
+    )
   }
 }
 
