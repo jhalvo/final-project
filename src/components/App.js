f01d2f14eb33d2854e22405a0946f829fe9b4aa5
Dave Kruse <dave@krusetech.com>
2019-04-27 13:22:00 -0500

    add some help

--- src/components/App.js
+++ src/components/App.js
@@ -9,13 +9,16 @@ class App extends Component {
     this.state = {
       loading: true,
       pokemonData: [],
+      nextUrl: null,
+      previousUrl: null,
+      selectedPokemon: {},
       dummyButtonClickCount: 0
     }
   }
 
   componentDidMount() {
     // setTimeout(callback, milliseconds) - just so you can see the loading animation ;)
-    setTimeout(() => {this.getData('https://pokeapi.co/api/v2/pokemon')}, 4000);
+    setTimeout(() => {this.getData('https://pokeapi.co/api/v2/pokemon')}, 3000);
   }
 
   getData(url) {
@@ -24,32 +27,68 @@ class App extends Component {
     .then(data => {
       this.setState({
         pokemonData: data.results,
-        loading: false
+        loading: false,
+        nextUrl: data.next,
+        previousUrl: data.previous
       })
     })
   }
 
-  handleDummyButtonClick = () => {
-    this.setState({
-      dummyButtonClickCount: this.state.dummyButtonClickCount + 1
-    })
+  getPokemonDetails(url) {
+    // implement a fetch call
+    // call setState() to update the selectedPokemon in state
+    
+  }
+
+  handleNextClick = () => {
+    this.getData(this.state.nextUrl);
+  }
+
+  handlePreviousClick = () => {
+    alert("make me work");
+    // pretty much the same as handleNextClick()
+  }
+
+  handlePokemonClick = (url) => {
+    alert("go get data from " + url);
+    // then update the slectedPokemon in state
+    // then use that data in render()
   }
 
   render() {
 
-    let pokemonComponentList = this.state.pokemonData.sort(
-      (a,b) => (a.name > b.name) ? 1 : -1
-    ).map(
-      (pokemon, index) => <div key={index}>{pokemon.name}</div>
+    // Loop over the list of pokemon 
+    // to create an array of components.
+    // Below in return(), we render the list by inserting {pokemonComponentList}
+    let pokemonComponentList = this.state.pokemonData.map(
+      (pokemon, index) => { 
+        return(
+          <div
+            key={index}
+            onClick={() => this.handlePokemonClick(pokemon.url) }
+            // we're using an anonymous function to pass a parameter to a callback function
+          >
+            {pokemon.name}
+          </div>
+        )
+      }
     )
 
     return (
       <div className="app">
-        <button onClick={this.handleDummyButtonClick}>
-          Dummy Button {this.state.dummyButtonClickCount}
-        </button>
+        <button onClick={this.handlePreviousClick}>Previous</button>
+        <button onClick={this.handleNextClick}>Next</button>
         <Loading visible={this.state.loading} />
-        {pokemonComponentList}
+
+        <div className="pokemon-list">
+          {pokemonComponentList}
+        </div>
+
+        {/* this would be a good chunk to turn into a component */}
+        <div className="selected-pokemon">
+          <div className="name">{this.state.selectedPokemon.name}</div>
+          {/* add some details */}
+        </div>
       </div>
     )
   }
